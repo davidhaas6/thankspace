ec73b4189f412af42ebedc18bd60a76cc216d41b
David Haas <davidhaas6@gmail.com>
2020-09-07 11:08:41 -0400

    side edits

--- config.py
+++ config.py
@@ -6,7 +6,8 @@ def load_json(filepath):
     return json.load(open(os.path.join(basedir, filepath)))
 
 class Config(object):
-    SECRET_KEY = os.urandom(32)
+    # SECRET_KEY = os.urandom(32)
+    SECRET_KEY = 'static but secret'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False  # Signals every time a change is made to the DB
